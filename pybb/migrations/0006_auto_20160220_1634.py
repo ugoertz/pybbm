06080e40d4add6d5fc22965d74986dc5f19c7c5c
Ulrich Goertz <ug@geometry.de>
2016-02-20 20:10:32 +0100

    Fix migration file.

--- pybb/migrations/0006_auto_20160220_1634.py
+++ pybb/migrations/0006_auto_20160220_1634.py
@@ -8,7 +8,7 @@ from django.conf import settings
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('pybb', '0006_merge'),
+        ('pybb', '0005_auto_20151108_1528'),
     ]
 
     operations = [
